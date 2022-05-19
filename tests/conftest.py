079fd344a863be97ddcdd956b0ac7c77dc5eafa5
evnskc <evnskc@gmail.com>
2022-05-19 12:59:40 +0300

    test: update pytest fixture for path to sword zip file

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,11 @@
+from pathlib import Path
+
 import pytest
 
 
 @pytest.fixture
 def sword():
-    return "KJV.zip"
+    return Path(__file__).resolve().parent.parent.joinpath('KJV.zip')
 
 
 @pytest.fixture
